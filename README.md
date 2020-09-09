82dc337a779d32d6d70d86511c6dbfbc35cb05c4
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-09-09 17:11:14 +0200

    Initial commit: calendar module

---
+++ README.md
@@ -0,0 +1,58 @@
+# [ms-teams-auto-call-recorder](https://github.com/BartlomiejRasztabiga/ms-teams-auto-call-recorder)
+
+# Built upon [MS-Teams-Auto-Joiner](https://github.com/atharva-lipare/MS-Teams-Auto-Joiner) and [Teams-Auto-Joiner](https://github.com/TobiasPankner/Teams-Auto-Joiner)
+
+# Calendar module (autoRecord.calendar.py)
+## This python script will automatically join and record [Microsoft Teams](https://www.microsoft.com/en-in/microsoft-365/microsoft-teams/group-chat-software) meetings appearing in your [Teams calendar](https://teams.microsoft.com/_#/calendarv2).
+
+## Features:
+- This python script will automatically open a chrome tab, enter your username, your password, open the calendar tab and then join a meeting if available.
+- After joining, it will record screen using Windows built in recording functionality [Game Bar](https://community.windows.com/en-us/stories/capture-and-share-videos-with-game-bar).
+- Before joining any meeting, it will by default turn off your camera and microphone.
+- After the organiser ends the meeting, it will open the calendar tab to look for new meetings and join the next meeting if available.
+- If the organiser does not end the meeting i.e. attendees are made to leave the meeting, the script will automatically leave the meeting after the number of participants present in the meeting falls below the minimum participants allowed set by the user (default is 10), but will rejoin the meeting if the join button is still visible on the calendar tab. To not rejoin the same meeting user must stop the script by CTRL+C.
+
+## Requirements:
+- [Python3](https://www.python.org/downloads/)
+- [Google Chrome browser](https://www.google.com/intl/en_in/chrome/)
+- Windows10
+
+## Prerequisites:
+### After cloning the repo, go in the repo directory and then follow below steps:
+- Step 1:
+    Install dependencies from [requirements.txt](requirements.txt):
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+- Step 2:
+    To set login credentials and minimum number of participants allowed, modify [config.json](config.json):
+    ```json
+    {
+    "username":"example@example.com",
+    "password":"example",
+    "minimumParticipants":10
+    }
+    ```
+## Usage:
+-   Run [autoRecord.py](autoRecord.py):
+    ```bash
+    python autoRecord.py
+    ```
+- The script might not work as expected if the browser is minimised i.e you may open other windows above it but do not minimize the chrome window.
+
+## Sample Scenario:
+- You have a meeting at 9 A.M, you may run the script anytime before 9 A.M. and the script will automatically join the meeting when the join button is available.
+- The script will then automatically join the next meeting scheduled at e.g.:- 10 A.M. AFTER the organiser ends the meeting.
+- If the organiser does not end the meeting i.e the organiser leaves the meeting instead of ending the meeting and tells the attendee to leave the meeting, the script will leave the meeting after the strength of the meeting falls down to 10 or any minimum value set by user.
+- To set a custom condition to automatically leave the meeting change the code on [line 105](https://github.com/atharva-lipare/MS-Teams-Auto-Joiner/blob/9a63c80b82bd7fea0211898cf0e5af4e889945bb/autoJoin.py#L105) of [autoJoin.py](autoJoin.py)
+
+## Troubleshooting:
+- If your internet connection is slow, increase the sleepDelay and timeOutDelay variable on [line 11](https://github.com/atharva-lipare/MS-Teams-Auto-Joiner/blob/9a63c80b82bd7fea0211898cf0e5af4e889945bb/autoJoin.py#L11) of [autoJoin.py](autoJoin.py). The default value is set to 2 and 60 seconds respectively.
+- If you get the following error:- ```selenium.common.exceptions.StaleElementReferenceException: Message: stale element reference: element is not attached to the page document``` ; This is due to a poor internet connection, so please increase the sleepDelay on [line 11](https://github.com/atharva-lipare/MS-Teams-Auto-Joiner/blob/9a63c80b82bd7fea0211898cf0e5af4e889945bb/autoJoin.py#L11) of [autoJoin.py](autoJoin.py).
+- Please refer to [Python Documentation](https://www.python.org) if facing issues while installing [python3](https://www.python.org/downloads/). 
+- Please refer to [Pip Documentation](https://pip.pypa.io/en/stable/) if facing issues while installing the dependencies in [requirements.txt](requirements.txt).
+
+## Contributing:
+- When contributing to this repository, feel free to discuss the change you wish to make via [Issue](https://github.com/atharva-lipare/MS-Teams-Auto-Joiner/issues) or [gitter chat room](https://gitter.im/MS-Teams-Auto-Joiner/community?utm_source=share-link&utm_medium=link&utm_campaign=share-link) before making a change.
+
---
+++ autoRecord.calendar.py
@@ -0,0 +1,186 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+from datetime import datetime
+from pynput.keyboard import Key, Controller
+import json
+import os
+
+sleepDelay = 2      # increase if you have a slow internet connection
+timeOutDelay = 30   # increase if you have a slow internet connection
+
+maxParticipants = curParticipants = 0
+minParticipants = 10
+
+opt = Options()
+opt.add_argument("--disable-infobars")
+opt.add_argument("start-maximized")
+opt.add_argument("--disable-extensions")
+# Pass the argument 1 to allow and 2 to block
+opt.add_experimental_option("prefs", { \
+    "profile.default_content_setting_values.media_stream_mic": 1, 
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.notifications": 1 
+})
+
+browser = webdriver.Chrome(ChromeDriverManager().install(),options=opt)
+
+class ScreenRecorder(object):
+    def start(self):
+        keyboard.press(Key.cmd)
+        keyboard.press(Key.alt)
+        keyboard.press('r')
+        keyboard.release(Key.cmd)
+        keyboard.release(Key.alt)
+        keyboard.release('r')
+    
+        
+    def stop(self):
+        keyboard.press(Key.cmd)
+        keyboard.press(Key.alt)
+        keyboard.press('r')
+        keyboard.release(Key.cmd)
+        keyboard.release(Key.alt)
+        keyboard.release('r')
+
+
+screenRecorder = ScreenRecorder()
+keyboard = Controller()
+
+
+def wait_and_find_ele_by_id(id, timeout=timeOutDelay):
+    sleep(sleepDelay)
+    for i in range(timeout):
+        try:
+            ele = browser.find_element_by_id(id)
+        except:
+            sleep(sleepDelay)
+        else:
+            return ele
+
+def wait_and_find_ele_by_link_text(text, timeout=timeOutDelay):
+    sleep(sleepDelay)
+    for i in range(timeout):
+        try:
+            ele = browser.find_element_by_link_text(text)
+        except:
+            sleep(sleepDelay)
+        else:
+            return ele
+
+def wait_and_find_element_by_xpath(xpath, timeout=timeOutDelay):
+    sleep(sleepDelay)
+    for i in range(timeout):
+        try:
+            ele = browser.find_element_by_xpath(xpath)
+        except:
+            sleep(sleepDelay)
+        else:
+            return ele
+
+def wait_and_find_elements_by_xpath(xpath, timeout=timeOutDelay):
+    sleep(sleepDelay)
+    for i in range(timeout):
+        try:
+            ele = browser.find_elements_by_xpath(xpath)
+        except:
+            sleep(sleepDelay)
+        else:
+            return ele
+
+def checkAndJoinMeeting():
+    global maxParticipants, curParticipants
+    joins = wait_and_find_elements_by_xpath('//button[.="Dołącz"]', 3)
+    if len(joins) == 0: # no meeting scheduled
+        return
+    joins[-1].click()   # join the latest meeting scheduled i.e if join buttons for 9 A.M and 10 A.M available, will join 10 A.M
+    elem = wait_and_find_element_by_xpath('//*[@id="page-content-wrapper"]/div[1]/div/calling-pre-join-screen/div/div/div[2]/div[1]/div[2]/div/div/section/div[2]/toggle-button[1]/div/button', timeOutDelay)
+    if elem.get_attribute('aria-pressed') == 'true': # turn off camera
+        elem.click()
+    elem = wait_and_find_element_by_xpath('//*[@id="preJoinAudioButton"]/div/button', timeOutDelay)
+    if elem.get_attribute('aria-pressed') == 'true': # turn off microphone
+        elem.click()
+    wait_and_find_element_by_xpath('//button[.="Dołącz teraz"]', timeOutDelay).click() # join meeting
+    print('Joined the meeting at {}'.format(datetime.now()))
+    screenRecorder.start()
+    sleep(60*5)
+    actions = ActionChains(browser)
+    rosterBtn = wait_and_find_element_by_xpath('//button[@id="roster-button"]', timeOutDelay)
+    actions.move_to_element(rosterBtn).click().perform()
+    print("clicked")
+    numStr = wait_and_find_elements_by_xpath('//span[@class="toggle-number"][@ng-if="::ctrl.enableRosterParticipantsLimit"]')
+    print(numStr)
+    if len(numStr) >= 2:
+        if numStr[1].text[1:-1] != '':
+            maxParticipants = curParticipants = int(numStr[1].text[1:-1])
+
+def checkAndEndOrLeaveOrJoinMeeting():
+    global maxParticipants, curParticipants
+    hangupBtn = wait_and_find_element_by_xpath('//button[@id="hangup-button"]', 2)
+    if hangupBtn != None: # currently in meeting
+        numStr = wait_and_find_elements_by_xpath('//span[@class="toggle-number"][@ng-if="::ctrl.enableRosterParticipantsLimit"]')
+        print(numStr)
+        if len(numStr) >= 2:
+            if numStr[1].text[1:-1] != '':
+                curParticipants = int(numStr[1].text[1:-1])
+            else :
+                actions = ActionChains(browser)
+                actions.move_to_element(wait_and_find_element_by_xpath('//button[@id="roster-button"]', timeOutDelay)).click().perform()
+        maxParticipants = max(maxParticipants, curParticipants)
+        if curParticipants <= minParticipants and curParticipants != 0:   # leaves the meeting automatically for given condition
+            hangupBtn = wait_and_find_element_by_xpath('//button[@id="hangup-button"]', 3)
+            actions = ActionChains(browser)
+            actions.move_to_element(hangupBtn).click().perform()
+            print('Left meeting at {}'.format(datetime.now()))
+            screenRecorder.stop()
+            browser.get('https://teams.microsoft.com/_#/calendarv2')    # open calendar tab
+        else :
+            return
+    else :        
+        maxParticipants = curParticipants = 0
+        browser.get('https://teams.microsoft.com/_#/calendarv2')
+        checkAndJoinMeeting()
+
+def init():
+    global minParticipants
+    browser.get('https://teams.microsoft.com/_#/calendarv2')    # open calendar tab in teams
+    sleep(1)
+    with open(os.path.join(os.path.curdir, 'config.json')) as f:
+        data = json.load(f)
+    minParticipants = data['minimumParticipants']
+    wait_and_find_ele_by_id('i0116', timeOutDelay).send_keys(data['username'])      # enter username
+    wait_and_find_ele_by_id('idSIButton9', timeOutDelay).click()                    # click next
+    wait_and_find_ele_by_id('i0118', timeOutDelay).send_keys(data['password'])      # enter password
+    wait_and_find_ele_by_id('idSIButton9', timeOutDelay).click()                    # click next
+    wait_and_find_ele_by_id('idSIButton9', timeOutDelay).click()                    # click yes to stay signed in 
+    wait_and_find_ele_by_link_text('Zamiast tego użyj aplikacji internetowej', timeOutDelay).click() # click use the web app instead link  
+    while wait_and_find_element_by_xpath('//button[@title="Zmień widok kalendarza"]', timeOutDelay) == None: #   wait for calendar tab to completely load
+        sleep(5)
+    while wait_and_find_element_by_xpath('//button[@title="Zmień widok kalendarza"]', timeOutDelay).get_attribute('name') != "Tydzień roboczy": # change calender work-week view to week view
+        wait_and_find_element_by_xpath('//button[@title="Zmień widok kalendarza"]', timeOutDelay).click()
+        wait_and_find_element_by_xpath('//button[@name="Tydzień roboczy"]', timeOutDelay).click()
+    print('Initialized Succesfully at {}'.format(datetime.now()))
+    checkAndJoinMeeting()
+
+def main():
+    global browser
+    try:
+        init()
+    except:
+        print('init failed, trying again')
+        main()
+    else:
+        while True:
+            try:
+                checkAndEndOrLeaveOrJoinMeeting()
+            except:
+                print('join meeting failed, trying again')
+                browser.get('https://teams.microsoft.com/_#/calendarv2')    # open calendar tab in teams
+            else:
+                sleep(3)
+
+if __name__ == "__main__":
+    main()
